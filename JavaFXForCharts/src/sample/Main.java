b403ef9f638dd3ffedaf7a656c70f26f5f1c3a5c
HisenLee <Hisen.lee.iverson@gmail.com>
2019-03-04 11:58:33 +0800

    first commit

---
+++ JavaFXForCharts/src/sample/Main.java
@@ -0,0 +1,313 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Side;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.chart.*;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        // 创建饼图
+//        createPieChart(primaryStage);
+
+        // 创建折线图
+//        createLineChart(primaryStage);
+
+        // 创建面积图
+//        createAreaChart(primaryStage);
+
+        // 创建气泡图
+//        createBubbleChart(primaryStage);
+
+        // 创建散点图
+//        createScatterChart(primaryStage);
+
+        // 创建柱状图
+        createBarChart(primaryStage);
+
+        primaryStage.setTitle("AllKindsOfCharts");
+        primaryStage.show();
+    }
+
+    /**
+     * 创建饼图
+     * @param primaryStage
+     */
+    private void createPieChart(Stage primaryStage) {
+        Group root = new Group();
+        Scene scene = new Scene(root, 500, 500);
+
+        ObservableList<PieChart.Data> pipDatas = FXCollections.observableArrayList(
+                new PieChart.Data("Banners", 13),
+                new PieChart.Data("Apples", 25),
+                new PieChart.Data("Oranges", 10),
+                new PieChart.Data("Pears", 22),
+                new PieChart.Data("Apples", 30)
+        );
+        final PieChart pieChart = new PieChart(pipDatas);
+        pieChart.setTitle("Fruits");
+        pieChart.setLabelLineLength(10);
+        pieChart.setLegendSide(Side.LEFT);
+        ((Group)scene.getRoot()).getChildren().add(pieChart);
+        primaryStage.setScene(scene);
+    }
+
+    /**
+     * 创建折线图
+     * @param primaryStage
+     */
+    private void createLineChart(Stage primaryStage) {
+        final CategoryAxis xAxis = new CategoryAxis();
+        final NumberAxis yAxis = new NumberAxis();
+        xAxis.setLabel("Month");
+        final LineChart<String,Number> lineChart =
+                new LineChart<String,Number>(xAxis,yAxis);
+
+        lineChart.setTitle("Stock Monitoring, 2010");
+
+        XYChart.Series series1 = new XYChart.Series();
+        series1.setName("Portfolio 1");
+
+        series1.getData().add(new XYChart.Data("Jan", 23));
+        series1.getData().add(new XYChart.Data("Feb", 14));
+        series1.getData().add(new XYChart.Data("Mar", 15));
+        series1.getData().add(new XYChart.Data("Apr", 24));
+        series1.getData().add(new XYChart.Data("May", 34));
+        series1.getData().add(new XYChart.Data("Jun", 36));
+        series1.getData().add(new XYChart.Data("Jul", 22));
+        series1.getData().add(new XYChart.Data("Aug", 45));
+        series1.getData().add(new XYChart.Data("Sep", 43));
+        series1.getData().add(new XYChart.Data("Oct", 17));
+        series1.getData().add(new XYChart.Data("Nov", 29));
+        series1.getData().add(new XYChart.Data("Dec", 25));
+
+        XYChart.Series series2 = new XYChart.Series();
+        series2.setName("Portfolio 2");
+        series2.getData().add(new XYChart.Data("Jan", 33));
+        series2.getData().add(new XYChart.Data("Feb", 34));
+        series2.getData().add(new XYChart.Data("Mar", 25));
+        series2.getData().add(new XYChart.Data("Apr", 44));
+        series2.getData().add(new XYChart.Data("May", 39));
+        series2.getData().add(new XYChart.Data("Jun", 16));
+        series2.getData().add(new XYChart.Data("Jul", 55));
+        series2.getData().add(new XYChart.Data("Aug", 54));
+        series2.getData().add(new XYChart.Data("Sep", 48));
+        series2.getData().add(new XYChart.Data("Oct", 27));
+        series2.getData().add(new XYChart.Data("Nov", 37));
+        series2.getData().add(new XYChart.Data("Dec", 29));
+
+        XYChart.Series series3 = new XYChart.Series();
+        series3.setName("Portfolio 3");
+        series3.getData().add(new XYChart.Data("Jan", 44));
+        series3.getData().add(new XYChart.Data("Feb", 35));
+        series3.getData().add(new XYChart.Data("Mar", 36));
+        series3.getData().add(new XYChart.Data("Apr", 33));
+        series3.getData().add(new XYChart.Data("May", 31));
+        series3.getData().add(new XYChart.Data("Jun", 26));
+        series3.getData().add(new XYChart.Data("Jul", 22));
+        series3.getData().add(new XYChart.Data("Aug", 25));
+        series3.getData().add(new XYChart.Data("Sep", 43));
+        series3.getData().add(new XYChart.Data("Oct", 44));
+        series3.getData().add(new XYChart.Data("Nov", 45));
+        series3.getData().add(new XYChart.Data("Dec", 44));
+
+        Scene scene  = new Scene(lineChart,800,600);
+        lineChart.getData().addAll(series1, series2, series3);
+        primaryStage.setScene(scene);
+    }
+
+    /**
+     * 创建面积图
+     */
+    private void createAreaChart(Stage primaryStage) {
+        final NumberAxis xAxis = new NumberAxis(1, 31, 1);
+        final NumberAxis yAxis = new NumberAxis();
+        final AreaChart<Number,Number> ac =
+                new AreaChart<>(xAxis,yAxis);
+        ac.setTitle("Temperature Monitoring (in Degrees C)");
+
+        XYChart.Series seriesApril= new XYChart.Series();
+        seriesApril.setName("April");
+        seriesApril.getData().add(new XYChart.Data(1, 4));
+        seriesApril.getData().add(new XYChart.Data(3, 10));
+        seriesApril.getData().add(new XYChart.Data(6, 15));
+        seriesApril.getData().add(new XYChart.Data(9, 8));
+        seriesApril.getData().add(new XYChart.Data(12, 5));
+        seriesApril.getData().add(new XYChart.Data(15, 18));
+        seriesApril.getData().add(new XYChart.Data(18, 15));
+        seriesApril.getData().add(new XYChart.Data(21, 13));
+        seriesApril.getData().add(new XYChart.Data(24, 19));
+        seriesApril.getData().add(new XYChart.Data(27, 21));
+        seriesApril.getData().add(new XYChart.Data(30, 21));
+
+        XYChart.Series seriesMay = new XYChart.Series();
+        seriesMay.setName("May");
+        seriesMay.getData().add(new XYChart.Data(1, 20));
+        seriesMay.getData().add(new XYChart.Data(3, 15));
+        seriesMay.getData().add(new XYChart.Data(6, 13));
+        seriesMay.getData().add(new XYChart.Data(9, 12));
+        seriesMay.getData().add(new XYChart.Data(12, 14));
+        seriesMay.getData().add(new XYChart.Data(15, 18));
+        seriesMay.getData().add(new XYChart.Data(18, 25));
+        seriesMay.getData().add(new XYChart.Data(21, 25));
+        seriesMay.getData().add(new XYChart.Data(24, 23));
+        seriesMay.getData().add(new XYChart.Data(27, 26));
+        seriesMay.getData().add(new XYChart.Data(31, 26));
+
+        Scene scene  = new Scene(ac,800,600);
+        ac.getData().addAll(seriesApril, seriesMay);
+        primaryStage.setScene(scene);
+    }
+
+    /**
+     * 创建气泡图
+     */
+    private void createBubbleChart(Stage primaryStage) {
+        final NumberAxis xAxis = new NumberAxis(1, 53, 4);
+        final NumberAxis yAxis = new NumberAxis(0, 80, 10);
+        final BubbleChart<Number,Number> blc = new
+                BubbleChart<>(xAxis,yAxis);
+        xAxis.setLabel("Week");
+        yAxis.setLabel("Product Budget");
+        blc.setTitle("Budget Monitoring");
+
+        XYChart.Series series1 = new XYChart.Series();
+        series1.setName("Product 1");
+        series1.getData().add(new XYChart.Data(3, 35));
+        series1.getData().add(new XYChart.Data(12, 60));
+        series1.getData().add(new XYChart.Data(15, 15));
+        series1.getData().add(new XYChart.Data(22, 30));
+        series1.getData().add(new XYChart.Data(28, 20));
+        series1.getData().add(new XYChart.Data(35, 41));
+        series1.getData().add(new XYChart.Data(42, 17));
+        series1.getData().add(new XYChart.Data(49, 30));
+
+        XYChart.Series series2 = new XYChart.Series();
+        series2.setName("Product 2");
+        series2.getData().add(new XYChart.Data(8, 15));
+        series2.getData().add(new XYChart.Data(13, 23));
+        series2.getData().add(new XYChart.Data(15, 45));
+        series2.getData().add(new XYChart.Data(24, 30));
+        series2.getData().add(new XYChart.Data(38, 78));
+        series2.getData().add(new XYChart.Data(40, 41));
+        series2.getData().add(new XYChart.Data(45, 57));
+        series2.getData().add(new XYChart.Data(47, 23));
+
+        Scene scene  = new Scene(blc);
+        blc.getData().addAll(series1, series2);
+        primaryStage.setScene(scene);
+    }
+
+    /**
+     * 创建散点图
+     * @param primaryStage
+     */
+    private void createScatterChart(Stage primaryStage) {
+        final NumberAxis xAxis = new NumberAxis(0, 10, 1);
+        final NumberAxis yAxis = new NumberAxis(-100, 500, 100);
+        final ScatterChart<Number,Number> sc = new
+                ScatterChart<>(xAxis,yAxis);
+        xAxis.setLabel("Age (years)");
+        yAxis.setLabel("Returns to date");
+        sc.setTitle("Investment Overview");
+
+        XYChart.Series series1 = new XYChart.Series();
+        series1.setName("Equities");
+        series1.getData().add(new XYChart.Data(4.2, 193.2));
+        series1.getData().add(new XYChart.Data(2.8, 33.6));
+        series1.getData().add(new XYChart.Data(6.2, 24.8));
+        series1.getData().add(new XYChart.Data(1, 14));
+        series1.getData().add(new XYChart.Data(1.2, 26.4));
+        series1.getData().add(new XYChart.Data(4.4, 114.4));
+        series1.getData().add(new XYChart.Data(8.5, 323));
+        series1.getData().add(new XYChart.Data(6.9, 289.8));
+        series1.getData().add(new XYChart.Data(9.9, 287.1));
+        series1.getData().add(new XYChart.Data(0.9, -9));
+        series1.getData().add(new XYChart.Data(3.2, 150.8));
+        series1.getData().add(new XYChart.Data(4.8, 20.8));
+        series1.getData().add(new XYChart.Data(7.3, -42.3));
+        series1.getData().add(new XYChart.Data(1.8, 81.4));
+        series1.getData().add(new XYChart.Data(7.3, 110.3));
+        series1.getData().add(new XYChart.Data(2.7, 41.2));
+
+        XYChart.Series series2 = new XYChart.Series();
+        series2.setName("Mutual funds");
+        series2.getData().add(new XYChart.Data(5.2, 229.2));
+        series2.getData().add(new XYChart.Data(2.4, 37.6));
+        series2.getData().add(new XYChart.Data(3.2, 49.8));
+        series2.getData().add(new XYChart.Data(1.8, 134));
+        series2.getData().add(new XYChart.Data(3.2, 236.2));
+        series2.getData().add(new XYChart.Data(7.4, 114.1));
+        series2.getData().add(new XYChart.Data(3.5, 323));
+        series2.getData().add(new XYChart.Data(9.3, 29.9));
+        series2.getData().add(new XYChart.Data(8.1, 287.4));
+
+        sc.getData().addAll(series1, series2);
+        Scene scene  = new Scene(sc, 500, 400);
+        primaryStage.setScene(scene);
+    }
+
+    /**
+     * 创建柱状图
+     * @param primaryStage
+     */
+    private void createBarChart(Stage primaryStage) {
+        final String austria = "Austria";
+        final String brazil = "Brazil";
+        final String france = "France";
+        final String italy = "Italy";
+        final String usa = "USA";
+
+        final NumberAxis xAxis = new NumberAxis();
+        final CategoryAxis yAxis = new CategoryAxis();
+        final BarChart<Number,String> bc =
+                new BarChart<>(xAxis,yAxis);
+        bc.setTitle("Country Summary");
+        xAxis.setLabel("Value");
+        xAxis.setTickLabelRotation(90);
+        yAxis.setLabel("Country");
+
+        XYChart.Series series1 = new XYChart.Series();
+        series1.setName("2003");
+        series1.getData().add(new XYChart.Data(25601.34, austria));
+        series1.getData().add(new XYChart.Data(20148.82, brazil));
+        series1.getData().add(new XYChart.Data(10000, france));
+        series1.getData().add(new XYChart.Data(35407.15, italy));
+        series1.getData().add(new XYChart.Data(12000, usa));
+
+        XYChart.Series series2 = new XYChart.Series();
+        series2.setName("2004");
+        series2.getData().add(new XYChart.Data(57401.85, austria));
+        series2.getData().add(new XYChart.Data(41941.19, brazil));
+        series2.getData().add(new XYChart.Data(45263.37, france));
+        series2.getData().add(new XYChart.Data(117320.16, italy));
+        series2.getData().add(new XYChart.Data(14845.27, usa));
+
+        XYChart.Series series3 = new XYChart.Series();
+        series3.setName("2005");
+        series3.getData().add(new XYChart.Data(45000.65, austria));
+        series3.getData().add(new XYChart.Data(44835.76, brazil));
+        series3.getData().add(new XYChart.Data(18722.18, france));
+        series3.getData().add(new XYChart.Data(17557.31, italy));
+        series3.getData().add(new XYChart.Data(92633.68, usa));
+
+        Scene scene  = new Scene(bc,800,600);
+        bc.getData().addAll(series1, series2, series3);
+        primaryStage.setScene(scene);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
